a6afc6de526f2a32168545335d3ad2d26c3ac40a
maclyn02 <mac.afonso@yahoo.com>
2019-03-17 08:45:45 +0000

    heroku setup

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -120,7 +120,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
