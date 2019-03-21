9509b1a0853b6da21a03ee46f03c1e0bf77ba1bf
maclyn02 <mac.afonso@yahoo.com>
2019-03-21 07:20:14 +0000

    Updated settings.py to find template path

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'helloapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'howdy/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
